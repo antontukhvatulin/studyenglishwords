d764e4fdd9aa983a19865b9702c30f0847898e7b
Anton Tukhvatulin <anton.tuhvatulin@gmail.com>
2023-02-12 14:32:23 +0300

    Added button Exit

--- app/src/main/java/ru/ajkon/studyenglishwords/MainActivity.kt
+++ app/src/main/java/ru/ajkon/studyenglishwords/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.ajkon.studyenglishwords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun onClickExitButton() {
+        finish()
+        exitProcess(0)
+    }
+
+
 }
